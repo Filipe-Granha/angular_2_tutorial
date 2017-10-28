2db486eee3267c86080f9994f28a393d6f2e583c
Filipe Granha <filipe@filipes-mbp.home>
2017-10-28 12:19:55 +0100

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+	{id: 1, name: "Mr Nice"},
+	{id: 2, name: "SuperTit"},
+	{id: 3, name: "Bombisimi"},
+	{id: 4, name: "Mice-O"},
+	{id: 5, name: "Black&pecker"},
+	{id: 6, name: "Dr IQ"},
+	{id: 7, name: "Magma"},
+	{id: 8, name: "Rubberman"},
+	{id: 9, name: "Tornado"},
+	{id: 10, name: "Hurricane"}
+	];
